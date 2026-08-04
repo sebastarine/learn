f0c4f82f7861583f77f8030a3423fbb545287baa
Sebastarine <sebastarinekov@gmail.com>
2026-08-04 12:49:08 +0000

    Create book.c

---
+++ book.c
@@ -0,0 +1,13 @@
+
+
+
+the basic part :
+
+The second book should be -> https://inria.hal.science/hal-02383654v2/file/modernC.pdf
+the third one should be -> https://www.cs.sfu.ca/~ashriram/Courses/CS295/assets/books/C_Book_2nd.pdf
+
+
+the networking part 
+https://hwang.lasierra.edu/~enoch/CPTG%20255%20Systems%20and%20Network%20Programming/Textbook/Chapter%201.pdf
+https://raw.githubusercontent.com/zwan074/technical-books/master/Advanced.Programming.in.the.UNIX.Environment.3rd.Edition.0321637739.pdf
+

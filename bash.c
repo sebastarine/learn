e5db8787c901ed3df571bc611e75dc6029ca7faf
sebastarine <sebastarinekov@gmail.com>
2026-08-05 15:23:15 +0600

    Feats

---
+++ bash.c
@@ -0,0 +1,17 @@
+
+
+
+
+
+learning bash_commands a lot 
+$ -> ps 
+
+PID TTY          TIME CMD
+633 pts/0    00:00:00 bash
+29757 pts/0    00:00:00 ps
+
+pid is th process id 
+663 is the pid of bash and 29757 is the pid of ps own , because when i type ps and then it will show some tasks means 
+this is also a process 
+$-> echo $$ this is also print the same process id , 663 , because its the pid of bash it self 
+

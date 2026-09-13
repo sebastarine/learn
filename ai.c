9f93fbac3b4d61cddaa06611eca647b43658c924
Sebastarine <sebastarinekov@gmail.com>
2026-09-13 14:10:57 +0000

    Create ai.c

---
+++ ai.c
@@ -0,0 +1,15 @@
+Deepseek->code generation and help 
+Qwen->socratic method for debugging  
+Deepseek-> disassembly 
+Deepseek->Hexdecimal + binary calculation in head 
+Gemini/chatgpt for other usecase 
+
+
+
+_______________________
+sudo pacman -S --needed base-devel git 
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+
